80eba48080631aa6ae7f3af7be8815277c88bfb0
zack <zengyangzhao@outlook.com>
2017-08-11 07:01:17 +0800

    selenium-webdriver 元素定位 xpath linkText partialLinkText name

---
+++ 2017-08-11.js
@@ -0,0 +1,19 @@
+/**
+ * selenium-webdriver 元素定位 xpath linkText partialLinkText name
+ */
+
+require('chromedriver');
+let webdriver = require('selenium-webdriver')
+let drvier = new webdriver.Builder().forBrowser('chrome').build();
+let By = webdriver.By;
+
+drvier.get("http://www.baidu.com");
+// drvier.findElement(By.xpath('//*[@id="u1"]/a[5]')).click();
+
+
+// drvier.findElement(By.linkText("新闻")).click();
+
+// drvier.findElement(By.partialLinkText('新闻')).click();
+
+drvier.findElement(By.name('wd')).sendKeys('1234')
+
