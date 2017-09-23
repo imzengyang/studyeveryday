80198d7f76b40d14f97e1b71b0dd1c223c3ccd8f
zack <zengyangzhao@outlook.com>
2017-09-23 09:29:11 +0800

    psot api

---
+++ 2017-09/2017-09-23.js
@@ -0,0 +1,18 @@
+/**
+ * axios  api : Post
+ * https://www.npmjs.com/package/axios
+ */
+
+
+let axios = require('axios');
+let assert = require('assert');
+
+axios.post("http://192.168.219.129:3000/api/v1/topics",{
+    accesstoken:"67cb2ef1-711a-4faa-ae70-0c1ba215643b",
+    title: "测试api post 请求。。。。。",
+    tab:"share",
+    content:"hahdhadahdhahfdfdfdasfadsasdasdfadsdf"
+}).then(function(res){
+    console.log(res.data)
+    assert.deepEqual(res.data.success, true);
+})
